ad9312df7962e1068a5a2e98f562c72c1e4e388b
sun-funny <mariesiushkova@gmail.com>
2026-01-13 16:23:10 +0300

    excel report mappings

--- namespace/ots_pr_spr/query_builder.py
+++ namespace/ots_pr_spr/query_builder.py
@@ -1,9 +1,9 @@
 ﻿from flask import session
 from sqlalchemy import and_, func, case
 from progSpros_back.functions.chart_data_functions_ps import apply_dynamic_filters
-from progSpros_back.functions.utility_functions_ps import create_filter_params, set_db_connection
+from progSpros_back.functions.utility_functions_ps import create_filter_params, set_db_connection, mapping
 from progSpros_back.model.db_models_ps import Prirost, PSDATA, reference_models, Otrasl, FedState, Regions, GroupPost, Contragent, StPotr, StGaz, PG, Dogovor, TU, Infr, VersProgn, Proizv
-from progSpros_back.model.mappings_ps import otr_mapping, vers_mapping, grpost_mapping, fo_mapping, region_mapping, yn_mapping
+from progSpros_back.model.mappings_ps import vers_mapping, yn_mapping
 from progSpros_back.database_ps import db
 
 def get_query(request, yearfrom, yearto, date):
@@ -54,6 +54,12 @@ def get_query(request, yearfrom, yearto, date):
         base_query = apply_dynamic_filters(base_query, PSDATA, filter_params, db, reference_models)
 
         sum_pr = request.args.get('sum_pr', 0, type=int)
+
+        # Мэппинги из справочников
+        otr_mapping = mapping(Otrasl)
+        grpost_mapping = mapping(GroupPost)
+        fo_mapping = mapping(FedState)
+        region_mapping = mapping(Regions)
 
         # Параметры Отрасль
         reverse_otr_mapping = {value: key for key, value in otr_mapping.items()}
