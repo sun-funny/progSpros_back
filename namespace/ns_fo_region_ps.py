4ce724e9f5e9f19391afd5cb2d16ea04f66d87b0
glocalchaos <this@perkv.ru>
2026-01-22 13:59:20 +0300

    fix cors

--- namespace/ns_fo_region_ps.py
+++ namespace/ns_fo_region_ps.py
@@ -46,7 +46,7 @@ group_regions_request_model = ns_fo_region_ps.model('GroupRegionsRequest', {
 })
 
 
-@ns_fo_region_ps.route('/fo-region', methods=['GET', 'POST', 'PUT'])
+@ns_fo_region_ps.route('/fo-region', methods=['GET', 'POST', 'PUT', 'OPTIONS'])
 @ns_fo_region_ps.response(200, 'Success')
 class FORegionDATA(Resource):
     @ns_fo_region_ps.doc(params={
@@ -219,8 +219,8 @@ class FORegionDATA(Resource):
 
     def options(self):
         origin = request.headers.get('Origin')
-        return {'Allow': 'POST'}, 200, {
+        return {'Allow': 'OPTIONS, POST, GET'}, 200, {
             'Access-Control-Allow-Origin': origin,
-            'Access-Control-Allow-Methods': 'POST, OPTIONS',
+            'Access-Control-Allow-Methods': 'POST, OPTIONS, GET',
             'Access-Control-Allow-Headers': 'Content-Type, Authorization'
         }
